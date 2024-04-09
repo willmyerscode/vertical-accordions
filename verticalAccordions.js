1f42dc7555d8ce58903c64fe3b62350ec6d1805f
Will Myers <56778753+willmyethewebsiteguy@users.noreply.github.com>
2024-04-08 23:20:06 -0500

    Update verticalAccordions.js

--- verticalAccordions.js
+++ verticalAccordions.js
@@ -14,19 +14,20 @@ class WMVerticalAccordion {
     return elem.dispatchEvent(event);
   };
   constructor(el){
-
     this.el = el;
     
-    let url = new URL(window.location.href);
-    let searchParams = new URLSearchParams(url.search);
-    this.paramActive = parseInt(searchParams.get('va-active'));
-
-    
-    this.openOnLoad = el.getAttribute('data-open') || '1';
-    this.openOnLoad = this.openOnLoad.trim();
-    this.openOnLoad =  parseInt(this.openOnLoad);
+    // let url = new URL(window.location.href);
+    // let searchParams = new URLSearchParams(url.search);
+    // this.paramActive = parseInt(searchParams.get('va-active'));
     
     this.accordionPanels = this.el.querySelectorAll('[data-wm-plugin="vertical-accordions"] .accordion-panel');
+    
+    // this.openOnLoad = el.getAttribute('data-open') || '1';
+    // this.openOnLoad = this.openOnLoad.trim();
+    // this.openOnLoad = parseInt(this.openOnLoad);
+    this.initialOpen = this.getInitialPanelIndexToOpen();
+    this.activePanel = null;
+    this.canOpen = true;
     
     this.accordionTitles = this.el.querySelectorAll('.accordion-title');
     this.component = this.el.querySelector('.vertical-accordions');
@@ -34,16 +35,19 @@ class WMVerticalAccordion {
     this.classicSections = this.el.querySelectorAll('.sqs-layout');
 
     this.init();
-  }
+  }  
   init () {
-    this.setActiveState();
     this.bindEvents();
     this.findSizes();
-    this.resizeEvent();
-    WMVerticalAccordion.emitEvent('wmVerticalAccordions:loaded');
+    this.openPanel(this.accordionPanels[this.initialOpen])
+    WMVerticalAccordion.emitEvent('wmVerticalAccordions:loaded', {
+      container: this.el
+    });
   }
   bindEvents() {
     this.addPluginLoadedListener();
+    this.resizeEvent();
+    this.addClickEvent();
   }
   addPluginLoadedListener() {
     const handleLoaded = () => {
@@ -53,44 +57,56 @@ class WMVerticalAccordion {
     }
     document.addEventListener('wmVerticalAccordions:loaded', handleLoaded)
   }
-
-  setActiveState(){
-    if (this.openOnLoad > this.accordionPanels.length) {
-      this.openOnLoad = this.accordionPanels.length;
+  openPanel(panel) {
+    if (this.canOpen && panel !== this.activePanel) {
+      this.canOpen = false;
+      this.removeActiveClasses();
+      panel.classList.add('panel-active');
+      const currentActivePanel = this.el.querySelector('.panel-active');
+      this.activePanel = this.el.querySelector('.panel-active');
+      this.allowFocus(this.activePanel)
+      this.scrollBack();
+      this.activePanel.addEventListener('transitionend', () => {
+        this.canOpen = true;
+        this.activePanel.removeEventListener('transitionend', null);
+      });
+    }
+  }
+  addClickEvent() {
+    const handleEvent = (e) => {
+      const panel = e.target.closest('.accordion-panel')
+      this.openPanel(panel)
     }
 
-     this.activePanel = this.openOnLoad - 1;
-
-    if (this.paramActive && this.paramActive <= this.accordionPanels.length) {
-      console.log(parseInt(this.paramActive));
-   console.log(this.paramActive);
-      this.activePanel = parseInt(this.paramActive) - 1;
-    }
-   
-    
-    this.accordionPanels[this.activePanel].classList.add('panel-active');
-    
-    this.accordionPanels.forEach((panel) => {
-      panel.addEventListener('click', function() {
-        this.removeActiveState();
-        panel.classList.add('panel-active');
-        const currentActivePanel = this.el.querySelector('.panel-active');
-          currentActivePanel.addEventListener('transitionend', () => {
-            currentActivePanel.removeEventListener('transitionend', null);
-            this.scrollBack();
-          });
-
-      }.bind(this));
+    this.accordionPanels.forEach(panel => {
+      panel.addEventListener('click', handleEvent)
     });
   }
 
-  removeActiveState(){
+  removeActiveClasses(){
     this.accordionPanels.forEach((panel) => {
       panel.classList.remove('panel-active');
+      this.preventFocusableElements(panel)
     });
-    
   }
+  getInitialPanelIndexToOpen() {
+    let url = new URL(window.location.href);
+    let searchParams = new URLSearchParams(url.search);
+    let searchParamActivePanel = searchParams.get('va-active');
+    let panelIndex = this.el.getAttribute('data-open') || '1';
+    if (searchParamActivePanel) {
+      panelIndex = searchParamActivePanel;
+    }
+      
+    panelIndex = parseInt(panelIndex.trim()) - 1;
 
+    if (panelIndex < 0) {
+      panelIndex = 0
+    } else if (panelIndex > this.accordionPanels.length) {
+      panelIndex = this.accordionPanels.length
+    }
+    return panelIndex;
+  }
   findSizes(){
     this.componentSizes = this.component.getBoundingClientRect();
     this.componentWidth = this.componentSizes.width;
@@ -113,7 +129,7 @@ class WMVerticalAccordion {
         }
         this.activeHeight = sectionHeight + 'px';
         this.component.style.setProperty('--va-active-height', this.activeHeight);
-        this.component.style.setProperty('--va-max-panel-height', maxHeight + 'px');
+        this.component.style.setProperty('--va-tallest-panel-height', maxHeight + 'px');
       });
     }
 
@@ -126,41 +142,47 @@ class WMVerticalAccordion {
         }
         this.activeHeight = sectionHeight + 'px';
         this.component.style.setProperty('--va-active-height', this.activeHeight);
-        this.component.style.setProperty('--va-max-panel-height', maxHeight + 'px');
+        this.component.style.setProperty('--va-tallest-panel-height', maxHeight + 'px');
     });
   }
     
   }
-
   resizeEvent(){
     window.addEventListener('resize', () => {
       this.findSizes();
     });
   }
-
   scrollBack() {
-  const prevPanel = document.querySelector('.panel-active').previousElementSibling;
-  const prevTitle = prevPanel?.querySelector('.accordion-title');
-
-  if (!prevTitle) return;
-  
-  const titleRect = prevTitle.getBoundingClientRect();
-  const openAccBottom = titleRect.bottom;
-  
-  if (openAccBottom < 0) {
-    const scrollBy = openAccBottom - 100;
-    window.scrollBy({
-      top: scrollBy,
-      behavior: 'smooth'
+    const prevPanel = document.querySelector('.panel-active').previousElementSibling;
+    const prevTitle = prevPanel?.querySelector('.accordion-title');
+    const rect = this.el.getBoundingClientRect();
+    const top = rect.top + window.pageYOffset;
+    if (!prevTitle) {
+      return;
+    } else if (rect.top <= 0) {
+      window.scrollTo({
+        top: top,
+        behavior: 'smooth'
+      });
+    }
+    
+  }
+  preventFocusableElements(panel) {
+    const focusableElements = panel.querySelectorAll('.accordion-content-wrapper a, .accordion-content-wrapper button, .accordion-content-wrapper input, .accordion-content-wrapper textarea, .accordion-content-wrapper select, .accordion-content-wrapper details, .accordion-content-wrapper [tabindex]:not([tabindex="-1"])');
+    focusableElements.forEach(el => {
+      el.setAttribute('tabindex', '-1');
     });
   }
+  allowFocus(panel) {
+    const focusableElements = panel.querySelectorAll('.accordion-content-wrapper a, .accordion-content-wrapper button, .accordion-content-wrapper input, .accordion-content-wrapper textarea, .accordion-content-wrapper select, .accordion-content-wrapper details');
+    focusableElements.forEach(el => {
+      el.removeAttribute('tabindex');
+    });
   }
 }
 
 
-
 (function () {
-
   function deepMerge (...objs) {
   	function getType (obj) {
   		return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
@@ -194,78 +216,132 @@ class WMVerticalAccordion {
   	return clone;
   
   }
-  const userSettings = window.wmVerticalAccordionSettings ? window.wmVerticalAccordionSettings : {};
+  const userSettings = 
+    window.wmVerticalAccordionSettings ? 
+    window.wmVerticalAccordionSettings : {};
   const defaultSettings = {
-    /*Placeholder Settings*/
+    titleTag: 'h3',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
-</svg>
-`
+</svg>`
   };
   const mergedSettings = deepMerge({}, defaultSettings, userSettings);
 
+  // function reloadSquarespaceScripts(el) {
+  //   const scripts = [];
+  //   const accordions = document.querySelectorAll('[data-wm-plugin="vertical-accordions"]');
+  //   function loadScripts() {
+  //     if (!scripts.length) return;
+  //     let hasLoaded = [];
+  //     for (let el of scripts){
+  //       if (hasLoaded.includes(el.src) || hasLoaded.includes(el.innerHTML) || el.type == 'application/json') continue;
+  //       const script = document.createElement('script');
+  //       script.src = el.src;
+  //       script.async = el.async;
+  
+  //       script.onload = () => {
+  //         //console.log(`${el.src} loaded successfuly`);
+  //       };
+  
+  //       script.onerror = () => {
+  //         //console.log(`Error occurred while loading ${el.src}`);
+  //       };
+  
+  //       if (el.innerHTML) {
+  //         eval(el.innerHTML);
+  //         hasLoaded.push(el.innerHTML)
+  //       } else {
+  //         document.body.appendChild(script);
+  //         hasLoaded.push(el.src)
+  //       }
+  //     }
+  //   };
+  //   function runAfterDOMLoaded(fn) {
+  //     if (document.readyState === 'loading') {  // Still loading
+  //       document.addEventListener('DOMContentLoaded', fn);
+  //     } else {  // `DOMContentLoaded` has already fired
+  //       fn();
+  //     }
+  //   }
+  //   let hasBkgVideos = false;
+  //   let hasListSection = false;
+  //   let hasGallerySection = false;
+  //   let hasBkgFx = false;
 
-  function reloadSquarespaceScripts(el) {
-    const scripts = [];
-    function loadScripts() {
-      if (!scripts.length) return;
-      let hasLoaded = [];
-      for (let el of scripts){
-        if (hasLoaded.includes(el.src) || hasLoaded.includes(el.innerHTML) || el.type == 'application/json') continue;
-        const script = document.createElement('script');
-        script.src = el.src;
-        script.async = el.async;
+
+  //   runAfterDOMLoaded(() => {
+  //     accordions.forEach(el => {
+  //       window.Squarespace?.initializeLayoutBlocks(Y, Y.one(el));
+  //       window.Squarespace?.initializeNativeVideo(Y, Y.one(el));
+  //       window.Squarespace?.initializePageContent(Y, Y.one(el));
+  //       if (el.querySelector('.section-background .sqs-video-background-native')) 
+  //       {
+  //         hasBkgVideos = true;
+  //         }
+  //       if (el.querySelector('.page-section.user-items-list-section')) {
+  //         hasListSection = true;
+  //       }
+  //       if (el.querySelector('.page-section.gallery-section')) {
+  //         hasGallerySection = true;
+  //       }
+  //       if (el.querySelector('.background-fx-canvas')) {
+  //         hasBkgFx = true;
+  //       }
+  //     })
+  //     if (hasBkgVideos || hasListSection || hasGallerySection || hasBkgFx) {
+  //       let sqsLoaderScript = document.querySelector('body > [src*="https://static1.squarespace.com/static/vta"]');
+  //       scripts.push(sqsLoaderScript)
+  //     }
   
-        script.onload = () => {
-          //console.log(`${el.src} loaded successfuly`);
-        };
+  //     loadScripts()
+  //   })
+  // }
+  function reloadSquarespaceScripts() {
+    const loadScript = (scriptSrc, async = true) => {
+      if (document.querySelector(`script[src="${scriptSrc}"]`)) return; // Already loaded
   
-        script.onerror = () => {
-          //console.log(`Error occurred while loading ${el.src}`);
-        };
+      const script = document.createElement('script');
+      script.src = scriptSrc;
+      script.async = async;
+      document.body.appendChild(script);
+    };
   
-        if (el.innerHTML) {
-          eval(el.innerHTML);
-          hasLoaded.push(el.innerHTML)
-        } else {
-          document.body.appendChild(script);
-          hasLoaded.push(el.src)
-        }
+    const executeInlineScript = (scriptContent) => {
+      // Consider creating a sandboxed environment for execution if possible
+      // Alternatively, ensure the content is from a trusted source
+      try {
+        new Function(scriptContent)();
+      } catch (error) {
+        console.error('Error executing inline script:', error);
       }
     };
-    function runAfterDOMLoaded(fn) {
-      if (document.readyState === 'loading') {  // Still loading
-        document.addEventListener('DOMContentLoaded', fn);
-      } else {  // `DOMContentLoaded` has already fired
-        fn();
-      }
-    }
-    console.log(el)
-    let hasBkgVideos = el.querySelectorAll('.section-background .sqs-video-background-native').length;
-    let hasListSection = el.querySelectorAll('.page-section.user-items-list-section').length;
-    let hasGallerySection = el.querySelectorAll('.page-section.gallery-section').length;
-    let hasBkgFx = el.querySelectorAll('.background-fx-canvas').length;
-
-
-    runAfterDOMLoaded(() => {
+  
+    const accordions = document.querySelectorAll('[data-wm-plugin="vertical-accordions"]');
+    accordions.forEach(el => {
       window.Squarespace?.initializeLayoutBlocks(Y, Y.one(el));
       window.Squarespace?.initializeNativeVideo(Y, Y.one(el));
       window.Squarespace?.initializePageContent(Y, Y.one(el));
-
-      console.log('dom loaded');
-       /*If Background Video or Gallery Section*/
-          console.log(hasGallerySection)
-      if (hasBkgVideos || hasListSection || hasGallerySection || hasBkgFx) {
-        console.log('add loader')
-        let sqsLoaderScript = document.querySelector('body > [src*="https://static1.squarespace.com/static/vta"]');
-        scripts.push(sqsLoaderScript)
+  
+      // Directly check and load scripts if needed
+      if (el.querySelector('.section-background .sqs-video-background-native') ||
+          el.querySelector('.page-section.user-items-list-section') ||
+          el.querySelector('.page-section.gallery-section') ||
+          el.querySelector('.background-fx-canvas')) {
+        const sqsScriptSrc = "https://static1.squarespace.com/static/vta"; // Example, adjust as needed
+        loadScript(sqsScriptSrc);
       }
   
-      loadScripts()
-    })
-    // window.addEventListener('DOMContentLoaded', () => {
-      
-    // })
+      // For inline scripts - assume they are safe and from a trusted source.
+      // Replace this with actual selectors and logic as needed.
+      el.querySelectorAll('script').forEach(script => {
+        if (script.type === 'application/json') return; // Skip JSON scripts
+        if (script.src) {
+          loadScript(script.src, script.async);
+        } else {
+          executeInlineScript(script.innerHTML);
+        }
+      });
+    });
   }
   async function getItemsFromCollection(path) {
     try {
@@ -362,7 +438,6 @@ class WMVerticalAccordion {
     }
 
     buildHTML(el, data);
-    reloadSquarespaceScripts(el);
   }
   function formatDate(dateNumber, options = {}) {
     // Use the user's current locale, or you can allow passing a specific locale as an argument
@@ -388,14 +463,56 @@ class WMVerticalAccordion {
     // Format the date using Intl.DateTimeFormat
     return new Intl.DateTimeFormat(userLocale, formatOptions).format(date);
   }
+  async function duplicateRootCssRule() {
+    const hasStyle = document.querySelector('style#wm-root-theme-duplicate');
+    if (!hasStyle) return;
+    
+    try {
+      // Fetch the CSS file
+      const response = await fetch('/site.css');
+      const cssText = await response.text();
+  
+      // Parse the CSS to find the second :root rule
+      const cssRules = cssText.split('}').map(rule => rule.trim() + '}'); // Split and reassemble CSS rules
+      const rootRules = cssRules.filter(rule => rule.startsWith(':root'));
+      if (rootRules.length < 2) {
+        console.error('Second :root rule not found');
+        return;
+      }
+  
+      // Duplicate and modify the rule
+      const newRuleText = rootRules[1].replace(':root', '[data-section-theme="white"]');
+      
+      // Append the new rule as an internal style sheet
+      const styleTag = document.createElement('style');
+      styleTag.textContent += newRuleText; // Use += in case you want to append multiple rules
+      styleTag.dataset.description = "Duplicated of the :root Color Theme styles"
+      styleTag.id = "wm-root-theme-duplicate"
+      document.head.prepend(styleTag);
+    } catch (error) {
+      console.error('Error fetching or duplicated :root CSS rule', error);
+    }
+  }
+  function emitEvent(type, detail = {}, elem = document) {
+    // Make sure there's an event type
+    if (!type) return;
 
+    // Create a new event
+    let event = new CustomEvent(type, {
+      bubbles: true,
+      cancelable: true,
+      detail: detail,
+    });
+
+    // Dispatch the event
+    return elem.dispatchEvent(event);
+  };
   function buildHTML(el, data) {
 
     // Get Data Attributes
     let panelCount = el.getAttribute('data-count') || '4';
     panelCount = panelCount.trim();
     panelCount = parseInt(panelCount);
-  
 
     let maxPanels = parseInt(8);
 
@@ -412,7 +529,10 @@ class WMVerticalAccordion {
           .map(
             item => `
               <div class="accordion-panel">
-                <div class="accordion-title"><h3>${(item.title)}</h3><div class="icon-wrapper">${(mergedSettings.icon)}</div></div>
+                <button class="accordion-title">
+                  <${mergedSettings.titleTag} class="text">${(item.title)}</${mergedSettings.titleTag}>
+                  <div class="icon-wrapper">${(mergedSettings.icon)}</div>
+                </button>
                 <div class="accordion-content-wrapper">
                   <div class="accordion-content">${(item.body)}</div>
                 </div>
@@ -427,12 +547,27 @@ class WMVerticalAccordion {
     el.wmVerticalAccordion = new WMVerticalAccordion(el);
   }
 
-  const pluginEls = document.querySelectorAll('[data-wm-plugin="vertical-accordions"]');
-  pluginEls.forEach(el => {
-    if (el.dataset.source) {
-      PluginBuilderFromSource(el);
-    } else {
-      PluginBuilderFromStackedElements(el);
-    }
-  });
+
+  window.addEventListener('wmVerticalAccordions:ready', () => {
+    reloadSquarespaceScripts();
+  })
+  window.wmVerticalAccordionsInit = async () => {
+    let pluginEls = document.querySelectorAll('[data-wm-plugin="vertical-accordions"]:not(.loaded)');
+    if (!pluginEls.length) return;
+    duplicateRootCssRule();
+
+    const buildPromises = Array.from(pluginEls).map(async (el) => {
+      el.classList.add('loaded');
+      if (el.dataset.source) {
+        await PluginBuilderFromSource(el); // Ensure we await each build process
+      }
+    });
+
+    // Wait for all PluginBuilderFromSource promises to resolve
+    await Promise.all(buildPromises);
+
+    emitEvent('wmVerticalAccordions:ready');
+  }
+
+  window.wmVerticalAccordionsInit();
 })();
